7c1f825117c859be6409e3e492d84b4b8871922e
Sibi Prabakaran <sibi@psibi.in>
2016-03-06 02:32:43 +0530

    Update test

--- Spec.hs
+++ Spec.hs
@@ -5,10 +5,6 @@ import Data.Either (isRight)
 main :: IO ()
 main = hspec $ do
   describe "openURI" $ do
-    it "returns Right data constructor for https" $ do
-     doc <- openURI "https://www.google.com"
-     doc `shouldSatisfy` isRight
-
     it "returns Right data constructor for http" $ do
      doc <- openURI "http://www.google.com"
      doc `shouldSatisfy` isRight
