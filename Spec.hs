0b25f85c212b156f7a95cc2edf95023f94d37645
Sibi Prabakaran <sibi@psibi.in>
2016-03-06 03:20:30 +0530

    Additional test added

--- Spec.hs
+++ Spec.hs
@@ -8,4 +8,19 @@ main = hspec $ do
     it "returns Right data constructor for http" $ do
      doc <- openURI "http://www.google.com"
      doc `shouldSatisfy` isRight
+         
+  describe "openAsTags" $ do
+    it "returns Right data constructor for http" $ do
+     doc <- openAsTags "http://www.google.com"
+     doc `shouldSatisfy` isRight
+
+  describe "openAsXML" $ do
+    it "returns Right data constructor for http" $ do
+     doc <- openAsXML "http://www.google.com"
+     doc `shouldSatisfy` isRight
+
+  describe "openAsFeed" $ do
+    it "returns Right data constructor for http" $ do
+     doc <- openAsFeed "http://www.google.com"
+     doc `shouldSatisfy` isRight         
 
